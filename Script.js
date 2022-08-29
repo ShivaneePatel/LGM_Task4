54aa3d10eea793581b24b1b19401c8b0f077f727
ShivaneePatel <86794788+ShivaneePatel@users.noreply.github.com>
2022-08-29 23:08:24 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,30 +1,22 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else {
-      display.innerText += item.id;
-    }
-  };
-});
-
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+let screen = document.getElementById("screen");
+let buttons = document.querySelectorAll("button");
+for(item of buttons){
+    item.addEventListener("click",function(event)
+    { 
+        buttonText = event.target.innerHTML;
+        console.log(buttonText);
+        switch(buttonText)
+        {
+        case "C" : screen.value = "";
+        break;
+        case "=" :screen.value = eval(screen.value);
+        break; 
+        case "←" : screen.value = screen.value.slice(0,-1);
+        break;
+        default :  screen.value += event.target.innerHTML;
+        break; 
+                       
+         }
+        
+    })
+}
